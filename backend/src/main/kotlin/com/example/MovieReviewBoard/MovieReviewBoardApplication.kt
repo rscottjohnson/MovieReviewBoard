84b0080c8bfdb6e2a6ac458812ca9114208949a0
rscottjohnson <rscottjohnson@acm.org>
2021-06-06 22:11:09 -0400

    Through securing helloworld endpoint

---
+++ backend/src/main/kotlin/com/example/MovieReviewBoard/MovieReviewBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.example.MovieReviewBoard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieReviewBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieReviewBoardApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/example/MovieReviewBoard/MovieReviewBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.MovieReviewBoard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieReviewBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
